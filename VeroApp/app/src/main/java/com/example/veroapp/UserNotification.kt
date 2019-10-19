e5b73d36d682f95ed78aad6aad1948c48ed7f31b
Tiago <tiago.spana@gmail.com>
2019-10-19 13:35:06 -0300

    #6 - cancel function

--- VeroApp/app/src/main/java/com/example/veroapp/UserNotification.kt
+++ VeroApp/app/src/main/java/com/example/veroapp/UserNotification.kt
@@ -14,8 +14,10 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import kotlinx.android.synthetic.main.activity_user_notification.*
 import org.json.JSONObject
 import kotlin.concurrent.thread
+import kotlin.reflect.jvm.internal.impl.resolve.scopes.MemberScope
 
 class UserNotification : AppCompatActivity() {
+    val database: AppDatabase =  AppDatabase.getInstance(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,51 +29,66 @@ class UserNotification : AppCompatActivity() {
 
         txtResult.text = "\t" + model.storeName + "\r\n\r\nCampos:\r\n\r\n" + TextUtils.join("\r\n", model.fields)
 
-
-        thread {
-            var database: AppDatabase
-            database =AppDatabase.getInstance(this)
-
+//        thread {
+//            database = AppDatabase.getInstance(this)
+//        }
 
 
-            run {
-                btnConfirmar.setOnClickListener {
-                    thread {
-                        var resp = HashMap<String, String>()
-                        var pessoaDao = database.pessoaDAO()
-                        model.fields.forEach {
-                            try {
-                                var field = it
-                                var value = pessoaDao.get()
-                                if (field == "endereco")
-                                    resp[field] = value.endereco
-                                if (field == "nome")
-                                    resp[field] = value.nome
-                            } catch (ex: Exception) {
-                                ex.printStackTrace()
-                            }
-                        }
-                        model.response = resp
-                        val url = getString(R.string.server_endpoint) + "api/info/" + model.id
-                        try {
-                            var header = HashMap<String, String>()
-                            header["Content-Type"] = "application/json"
-                            var json = mapper.convertValue<Map<String,Any>>(model)//mapper.writeValueAsBytes(model)
-                            var result = khttp.post(url, json = json, headers = header)
-                            var content = result.text
-                            if(content ==""){
-
-                            }
-                        } catch (ex: Exception) {
-                            ex.printStackTrace()
-                        }
-                        this.finishActivity(0)
+        btnConfirmar.setOnClickListener {
+            thread {
+                var resp = HashMap<String, String>()
+                var pessoaDao = database.pessoaDAO()
+                model.fields.forEach {
+                    try {
+                        var field = it
+                        var value = pessoaDao.get()
+                        if (field == "endereco")
+                            resp[field] = value.endereco
+                        if (field == "nome")
+                            resp[field] = value.nome
+                    } catch (ex: Exception) {
+                        ex.printStackTrace()
                     }
                 }
+                model.response = resp
+                val url = getString(R.string.server_endpoint) + "api/info/" + model.id
+                try {
+                    var header = HashMap<String, String>()
+                    header["Content-Type"] = "application/json"
+                    var json = mapper.convertValue<Map<String, Any>>(model)//mapper.writeValueAsBytes(model)
+                    var result = khttp.post(url, json = json, headers = header)
+                    var content = result.text
+                    if (content == "") {
+
+                    }
+                } catch (ex: Exception) {
+                    ex.printStackTrace()
+                }
+                this.finish()
             }
         }
+
         btnCancelar.setOnClickListener {
-            this.finishActivity(0);
+            thread {
+//                var database: AppDatabase
+//                database = AppDatabase.getInstance(this)
+                var pessoaDao = database.pessoaDAO()
+                val url = getString(R.string.server_endpoint) + "api/info/" + model.id
+                try {
+                    var header = HashMap<String, String>()
+                    header["Content-Type"] = "application/json"
+                    var json = mapper.convertValue<Map<String, Any>>(model)//mapper.writeValueAsBytes(model)
+                    var result = khttp.post(url, json = json, headers = header)
+                    var content = result.text
+                    if (content == "") {
+
+                    }
+                } catch (ex: Exception) {
+                    ex.printStackTrace()
+                }
+                this.finish();
+            }
+
         }
     }
 }
